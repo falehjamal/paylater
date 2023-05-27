f347018a2348db8b748f8cb80ff5124a30d85f39
falehjamaluddin <falehjamaluddin@gmail.com>
2023-05-28 01:49:40 +0700

    coba perubahan

--- index.php
+++ index.php
@@ -14,8 +14,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
-
 	<style type="text/css">
 		html,body{
 			margin: 0;
@@ -39,6 +37,7 @@
 </head>
 <body>
 
+<!-- ini perubahan  -->
 
 
 	<div class="jumbotron">
